22d10bc1649e9e1d4d7cfd793e3a5312682e6424
ForFrame <b.sheewan@gmail.com>
2012-11-03 17:01:25 +0700

    update popup page

--- SeniorGeneous/gen/samcom/example/senoirandroid/R.java
+++ SeniorGeneous/gen/samcom/example/senoirandroid/R.java
@@ -86,9 +86,9 @@ public final class R {
         public static final int wrong=0x7f020048;
     }
     public static final class id {
-        public static final int LogRegisBt=0x7f08003f;
-        public static final int LoginBt=0x7f080033;
-        public static final int board=0x7f080045;
+        public static final int LogRegisBt=0x7f080041;
+        public static final int LoginBt=0x7f08003e;
+        public static final int board=0x7f080046;
         public static final int boardbutton=0x7f08000f;
         public static final int buttonnumber1=0x7f080011;
         public static final int buttonnumber10=0x7f08001a;
@@ -100,8 +100,9 @@ public final class R {
         public static final int buttonnumber7=0x7f080017;
         public static final int buttonnumber8=0x7f080018;
         public static final int buttonnumber9=0x7f080019;
-        public static final int calendar=0x7f080046;
+        public static final int calendar=0x7f080047;
         public static final int calendarbutton=0x7f080010;
+        public static final int checkContinueLogin=0x7f080040;
         public static final int countbutton=0x7f080030;
         public static final int counttable1=0x7f08001b;
         public static final int counttable10=0x7f080024;
@@ -118,25 +119,25 @@ public final class R {
         public static final int hospitalbutton=0x7f08000d;
         public static final int housebutton=0x7f08000e;
         public static final int layout_dialog=0x7f080000;
-        public static final int layout_popup_login=0x7f080031;
-        public static final int login1=0x7f08003b;
-        public static final int maintofarm1=0x7f080035;
-        public static final int maintohospital1=0x7f080039;
-        public static final int maintohouse1=0x7f08003a;
-        public static final int maintomarket1=0x7f080036;
-        public static final int maintopolice1=0x7f080037;
-        public static final int maintoschool=0x7f080038;
-        public static final int maintoschool1=0x7f08003d;
-        public static final int marketbutton=0x7f08003e;
-        public static final int menu_settings=0x7f08004a;
-        public static final int playBt=0x7f080040;
-        public static final int policebutton=0x7f080041;
-        public static final int regisBt=0x7f080034;
-        public static final int schoolbutton=0x7f080042;
-        public static final int schoolbutton1=0x7f080043;
-        public static final int schoollevel1=0x7f080047;
-        public static final int schoollevel2=0x7f080049;
-        public static final int schoollevel3=0x7f080048;
+        public static final int layout_popup_login=0x7f08003c;
+        public static final int login1=0x7f080037;
+        public static final int maintofarm1=0x7f080031;
+        public static final int maintohospital1=0x7f080035;
+        public static final int maintohouse1=0x7f080036;
+        public static final int maintomarket1=0x7f080032;
+        public static final int maintopolice1=0x7f080033;
+        public static final int maintoschool=0x7f080034;
+        public static final int maintoschool1=0x7f080039;
+        public static final int marketbutton=0x7f08003a;
+        public static final int menu_settings=0x7f08004b;
+        public static final int playBt=0x7f080042;
+        public static final int policebutton=0x7f08003b;
+        public static final int regisBt=0x7f08003f;
+        public static final int schoolbutton=0x7f080043;
+        public static final int schoolbutton1=0x7f080044;
+        public static final int schoollevel1=0x7f080048;
+        public static final int schoollevel2=0x7f08004a;
+        public static final int schoollevel3=0x7f080049;
         public static final int scoreback=0x7f08000a;
         public static final int scorehome=0x7f080008;
         public static final int scorenext=0x7f080009;
@@ -157,10 +158,10 @@ public final class R {
         public static final int star3=0x7f080005;
         public static final int star4=0x7f080006;
         public static final int star5=0x7f080007;
-        public static final int table=0x7f080044;
-        public static final int textUser=0x7f08003c;
+        public static final int table=0x7f080045;
+        public static final int textUser=0x7f080038;
         public static final int textView1=0x7f080001;
-        public static final int usertext=0x7f080032;
+        public static final int usertext=0x7f08003d;
     }
     public static final class layout {
         public static final int activity_dialog_score_sclv1g1=0x7f030000;
@@ -173,18 +174,17 @@ public final class R {
         public static final int activity_l1_sc_board=0x7f030007;
         public static final int activity_l1_sc_calendar=0x7f030008;
         public static final int activity_l1_sc_count=0x7f030009;
-        public static final int activity_login_popup=0x7f03000a;
-        public static final int activity_main=0x7f03000b;
-        public static final int activity_main_level1=0x7f03000c;
-        public static final int activity_market=0x7f03000d;
-        public static final int activity_market_level1=0x7f03000e;
-        public static final int activity_play_popup=0x7f03000f;
-        public static final int activity_police=0x7f030010;
-        public static final int activity_police_level1=0x7f030011;
-        public static final int activity_popup_login=0x7f030012;
-        public static final int activity_school=0x7f030013;
-        public static final int activity_school_level1=0x7f030014;
-        public static final int activity_select_school_level=0x7f030015;
+        public static final int activity_main=0x7f03000a;
+        public static final int activity_main_level1=0x7f03000b;
+        public static final int activity_market=0x7f03000c;
+        public static final int activity_market_level1=0x7f03000d;
+        public static final int activity_police=0x7f03000e;
+        public static final int activity_police_level1=0x7f03000f;
+        public static final int activity_popup_login=0x7f030010;
+        public static final int activity_popup_play=0x7f030011;
+        public static final int activity_school=0x7f030012;
+        public static final int activity_school_level1=0x7f030013;
+        public static final int activity_select_school_level=0x7f030014;
     }
     public static final class menu {
         public static final int activity_main=0x7f070000;
@@ -194,6 +194,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f050000;
+        public static final int continue_login=0x7f05000b;
         public static final int farm_intent=0x7f050005;
         public static final int hospital_intent=0x7f050006;
         public static final int house_intent=0x7f050008;
--- SeniorGeneous/src/samcom/example/senoirandroid/Main.java
+++ SeniorGeneous/src/samcom/example/senoirandroid/Main.java
@@ -3,13 +3,18 @@ package samcom.example.senoirandroid;
 import android.app.Activity;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import java.util.Date;
+import java.text.*;
 
 public class Main extends Activity {
 String CurrentUser;
@@ -28,9 +33,9 @@ SQLiteDatabase db;
 		myDBClass myDb = new myDBClass(this);
 		myDb.getWritableDatabase();
 		
-		// SelectCurrentUser(); check MAX(No)loginStatus table on Status == 'N' or null -> null =='Y' -> name 
+		// SelectCurrentUser(); check MAX(No)loginStatus table on Status == 'Logout'-> No or 'Y' -> name 
 		CurrentUser = myDb.SelectCurrentUser();
-		if(CurrentUser == null){
+		if(CurrentUser.equals("Logout")){
 			showLoginPopup();
 		}
 		
@@ -120,7 +125,133 @@ SQLiteDatabase db;
 	}
 
 	void showLoginPopup(){
+		final Dialog LoginPop = new Dialog(context2);
+		LoginPop.setContentView(R.layout.activity_login_popup);
 		
+		final myDBClass myDb = new myDBClass(this);
+		myDb.getWritableDatabase();
+				
+		final boolean checkUser;
+		final String username;
+		final Date d = new Date();
+		final boolean continueLoginState;
+		
+		Button LoginBt = (Button)LoginPop.findViewById(R.id.LoginBt);
+		LoginBt.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				EditText user = (EditText)LoginPop.findViewById(R.id.usertext);
+				username = user.getText().toString();
+				
+				CheckBox checkbox = (CheckBox)LoginPop.findViewById(R.id.checkContinueLogin);
+				if(checkbox.isChecked())
+				{
+					continueLoginState = true;
+
+				}
+				//check user info if got -> insert status table(name,date) ,no -> message Toast 
+				checkUser = myDb.checkUserInfo(username);
+				if(checkUser == true){
+					LoginPop.dismiss();
+					CurrentUser = username;
+					
+					myDb.InsertCurrent(CurrentUser,d,continueLoginState);
+					TextView result = (TextView) findViewById(R.id.textUser);
+					result.setText(CurrentUser);
+				}
+				else{
+					//String strTxt = editT1.getText().toString();              
+					Toast.makeText(Main.this, username + " : not in user info", Toast.LENGTH_LONG).show(); 
+				}
+				
+				//CurrentUser = user.getText().toString();
+				
+			}
+		});
+		
+		Button RegisBt = (Button)LoginPop.findViewById(R.id.regisBt);
+		LoginBt.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			String name;	
+				EditText user = (EditText)LoginPop.findViewById(R.id.usertext);
+				name = user.getText().toString();
+				
+				LoginPop.dismiss();
+				showRegisPopup(name);				
+				//CurrentUser = user.getText().toString();
+				
+			}
+		});
+		
+			
+		LoginPop.show();
+	}
+	void showRegisPopup(String inputname){
+		
+		final Dialog RegisPop = new Dialog(context2);
+		RegisPop.setContentView(R.layout.activity_login_popup);
+		
+		final myDBClass myDb = new myDBClass(this);
+		myDb.getWritableDatabase();
+				
+		final boolean checkUser;
+		final String username;
+		final Date d = new Date();
+		final boolean continueLoginState;
+		
+		Button LoginBt = (Button)LoginPop.findViewById(R.id.LoginBt);
+		LoginBt.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+				EditText user = (EditText)LoginPop.findViewById(R.id.usertext);
+				username = user.getText().toString();
+				
+				CheckBox checkbox = (CheckBox)LoginPop.findViewById(R.id.checkContinueLogin);
+				if(checkbox.isChecked())
+				{
+					continueLoginState = true;
+
+				}
+				//check user info if got -> insert status table(name,date) ,no -> message Toast 
+				checkUser = myDb.checkUserInfo(username);
+				if(checkUser == true){
+					LoginPop.dismiss();
+					CurrentUser = username;
+					
+					myDb.InsertCurrent(CurrentUser,d,continueLoginState);
+					TextView result = (TextView) findViewById(R.id.textUser);
+					result.setText(CurrentUser);
+				}
+				else{
+					//String strTxt = editT1.getText().toString();              
+					Toast.makeText(Main.this, username + " : not in user info", Toast.LENGTH_LONG).show(); 
+				}
+				
+				//CurrentUser = user.getText().toString();
+				
+			}
+		});
+		
+		Button RegisBt = (Button)LoginPop.findViewById(R.id.regisBt);
+		LoginBt.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+			String name;	
+				EditText user = (EditText)LoginPop.findViewById(R.id.usertext);
+				name = user.getText().toString();
+				
+				LoginPop.dismiss();
+				showRegisPopup(name);				
+				//CurrentUser = user.getText().toString();
+				
+			}
+		});
+		
+			
+		LoginPop.show();
 	}
 	void popUpLogIn(){
 		final Dialog popLog = new Dialog(context2);
@@ -128,21 +259,29 @@ SQLiteDatabase db;
 		
 		final myDBClass myDb = new myDBClass(this);
 		myDb.getWritableDatabase();
-				
-		Button LoginBt = (Button)popLog.findViewById(R.id.logbnt);
+			
+		Button PlayBt = (Button)popLog.findViewById(R.id.playBt);
+		PlayBt.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				popLog.dismiss();
+				myDb.InsertCurrent("Guest");
+				TextView result = (TextView) findViewById(R.id.textUser);
+				result.setVisibility(TextView.INVISIBLE);
+				Button LoginBt = (Button) findViewById(R.id.login1);
+				result.setVisibility(Button.INVISIBLE);
+			}
+		});
+		
+		Button LoginBt = (Button)popLog.findViewById(R.id.LogRegisBt);
 		LoginBt.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				popLog.dismiss();
-				EditText user = (EditText)popLog.findViewById(R.id.userTxt);
-				CurrentUser = user.getText().toString();
-				myDb.InsertCurrent(CurrentUser);
-				TextView result = (TextView) findViewById(R.id.textUser);
-				result.setText(user.getText().toString());
+				showLoginPopup();
 			}
 		});
-		
-		
+	
 		popLog.show();
 		
 		
